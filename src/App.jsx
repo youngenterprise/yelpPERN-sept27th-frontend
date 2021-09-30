8773d3984cc856b1aa62ba710488224b1e19cba4
Nakaw <nakawyoung@gmail.com>
2021-09-30 16:28:51 -0400

    Added routes and finished app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { RestaurantContextProvider } from './context/RestaurantsContext';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
+
 const App = () =>{
   return (
     <RestaurantContextProvider>
-
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+            <Route exact path="/restaurants/:id" component={RestaurantDetailPage}/>
+          </Switch>
+        </Router>
+      </div>
     </RestaurantContextProvider>
   )
 }
